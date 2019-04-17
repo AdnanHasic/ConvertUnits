a749dc7b1adb59ea3e32e7518de4cb1d93d201fb
adnan hasic <hasic.adnan.ado@gmail.com>
2019-04-17 02:36:13 +0200

    Napravio metodu za konvertovanje inches to centimeters

--- src/FormsForCalculating.java
+++ src/FormsForCalculating.java
@@ -1,46 +1,56 @@
 
 public class FormsForCalculating {
-	
+
 	private double result;
 
 	public FormsForCalculating() {
-		
-	}
-	
 
-	
-	public void kilogramsToLbs () {
-		
+	}
+
+	public void kilogramsToLbs() {
+
 		System.out.println("Enter the weight in kilograms : ");
-		
+
 		double kilograms = Convereter.unos.nextDouble();
-		
+
 		this.result = kilograms * 2.2046;
-		
+
 		System.out.println("kg " + kilograms + " to " + "lbs = " + result);
-		
+
 	}
-    
-	public void lbsToKilograms () {
-		
+
+	public void lbsToKilograms() {
+
 		System.out.println("Enter the weight in pounds : ");
-		
+
 		double pounds = Convereter.unos.nextDouble();
-		
+
 		this.result = pounds / 2.2046;
-		
+
 		System.out.println("lbs " + pounds + " to " + " kilograms = " + result);
-		
+
 	}
-	
-	public void centimetersToInches () {
-		
+
+	public void centimetersToInches() {
+
 		System.out.println("Enter the length in centimeters : ");
-		
+
 		double centimeters = Convereter.unos.nextDouble();
-		
+
 		this.result = centimeters * 0.39370;
-		
-		System.out.println("cm " + centimeters + " to inches " + result);
+
+		System.out.println("cm " + centimeters + " to inches = " + result);
+	}
+
+	public void inchesToCentimeters() {
+
+		System.out.println("Enter the length in centimeters : ");
+
+		double inches = Convereter.unos.nextDouble();
+
+		this.result = inches / 0.39370;
+
+		System.out.println("inches " + inches + " to centimeters = " + result);
+
 	}
 }
